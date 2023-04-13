e52eb2f0f9e37d284801569cd50cfb9d9492b7ed
jaehwan-lee-benja <designerbenja@gmail.com>
2023-04-13 21:13:34 +0900

    running!

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <p>hi</p>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { supabase } from './lib/supabaseClient.js'
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     // HelloWorld
+//   }
+// }
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+        const a = await supabase
+            .from('toDo')
+            .select()
+        console.log("working! - fetchData()");
+        console.log("a = ", a);
+    }
   }
 }
+
 </script>
 
 <style>
