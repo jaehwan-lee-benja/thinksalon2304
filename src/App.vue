b3c72aed12ca5f3802de22bb9c18c604e558452a
jaehwan-lee-benja <designerbenja@gmail.com>
2023-04-13 21:30:16 +0900

    clean version - running2

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,33 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <p>hi</p>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <ul>
     <li v-for="todo in todos" :key="todo.id">
-        <input v-model="todo.text" placeholder="Type here" :readonly="false">
+        <input v-model="todo.text">
     </li>
   </ul>
 </template>
 
 <script>
-import { supabase } from './lib/supabaseClient.js'
-// import HelloWorld from './components/HelloWorld.vue'
+  import { supabase } from './lib/supabaseClient.js'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     // HelloWorld
-//   }
-// }
-
-export default {
-  data() {
-      return {
-        todos: [],
+  export default {
+    data() {
+        return {
+          todos: [],
+        }
+    },
+    mounted() {
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+          const a = await supabase
+              .from('toDo')
+              .select()
+          const { data } = a;
+          this.todos = data;
+          console.log("working! - fetchData()");
+          console.log("a = ", a);
       }
-  },
-  mounted() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-        const a = await supabase
-            .from('toDo')
-            .select()
-        const { data } = a;
-        this.todos = data;
-        console.log("working! - fetchData()");
-        console.log("a = ", a);
     }
   }
-}
-
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
