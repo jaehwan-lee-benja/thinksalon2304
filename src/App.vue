34efce91ac1af19db4f6f901bb5624dec0f932a4
jaehwan-lee-benja <designerbenja@gmail.com>
2023-06-01 20:35:19 +0900

    미팅 전 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <nav>
-    <router-link to="/reading">읽기모드</router-link> |
-    <router-link to="/editing">편집모드</router-link>
+      <router-link to="/reading">읽기모드</router-link> 
+      <router-link to="/editing">편집모드</router-link>
   </nav>
+  <!-- <button @click="handlingMode"> mode </button>
+
+    <div v-if="mode">
+      <nav>
+        <router-link to="/reading">읽기모드</router-link> 
+      </nav>
+    </div>
+    <div v-else> 
+      <nav>
+        <router-link to="/editing">편집모드</router-link>
+      </nav>
+    </div> -->
   <router-view /> <!-- 실제 페이지가 보여질 위치 -->
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mode: true
+    }
+  },
+  methods: {
+    handlingMode() {
+      this.readingMode = !this.readingMode
+      console.log("this.readingMode = ", this.readingMode);
+    }
+  }
+}
+</script>
