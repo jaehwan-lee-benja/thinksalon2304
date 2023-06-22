466a5031d9c54b35bfb65df1856c7503cac5347c
jaehwan-lee-benja <designerbenja@gmail.com>
2023-06-22 12:10:03 +0900

    230612 미팅 커밋

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,12 @@
         <span v-if="mode">'편집하기' 버튼을 누르면, 내용을 편집할 수 있습니다.</span>
         <span v-else>'뒤로가기' 버튼을 누르면, 편집모드가 종료됩니다.(저장되지 않음)</span>
       </div>
+      <span>email:</span><input v-model="email">
+      <span>password:</span><input v-model="password">
+      <button @click="signup">sing-up</button>
+      <button @click="login">login</button>
+      <button @click="signout">sign-out</button>
+      <button @click="getSession">getSession</button>
     </div>
     <div>
       <ReadingView v-if="mode" />
@@ -24,13 +30,16 @@
 <script>
 import ReadingView from './views/ReadingView.vue'
 import EditingView from './views/EditingView.vue'
+import { supabase } from './lib/supabaseClient.js'
 
 export default {
   data() {
     return {
       mode: true,
       appBodyGrid: 'appBodyGrid',
-      menuDiv: 'menuDiv'
+      menuDiv: 'menuDiv',
+      email: '',
+      password: '',
     }
   },
   components: {
@@ -40,7 +49,37 @@ export default {
   methods: {
     handlingMode() {
       this.mode = !this.mode
-    }
+    },
+    async signup() {
+      const { data, error } = await supabase.auth.signUp({
+        email: this.email,
+        password: this.password,
+      })
+    console.log("email = ", this.email);
+    console.log("password = ", this.password);
+    console.log("data = ", data);
+    console.log("error = ", error);
+    },
+    async login() {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: this.email,
+        password: this.password,
+      })
+    console.log("data = ", data);
+    console.log("error = ", error);
+    
+    },
+    async getSession() {
+      const { data, error } = await supabase.auth.getSession()
+      console.log("session = ", data)
+      console.log("error = ", error);
+    },
+    async signout() {
+      const { error } = await supabase.auth.signOut()
+      console.log("error = ", error);
+    
+    },
+    
   }
 }
 </script>
