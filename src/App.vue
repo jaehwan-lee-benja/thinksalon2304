ebdba010cd58370b394873d00ba63c327952e95d
jaehwan-lee-benja <designerbenja@gmail.com>
2023-06-30 15:47:53 +0900

    미팅 후 커밋

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@
         <!-- <button @click="getSession">getSession</button> -->
       </div>
 
-
-      <LoginView />
+      <button v-if="loginMode" @click="loginGoogle">구글 로그인</button>
+      <button v-else @click="signout">sign-out</button>
 
     </div>
     <div>
@@ -31,12 +31,13 @@
 <script>
 import ReadingView from './views/ReadingView.vue'
 import EditingView from './views/EditingView.vue'
-import LoginView from './views/LoginView.vue'
 import { supabase } from './lib/supabaseClient.js'
 
 export default {
   data() {
     return {
+      users: '',
+      loginMode: true,
       mode: true,
       appBodyGrid: 'appBodyGrid',
       menuDiv: 'menuDiv',
@@ -49,15 +50,113 @@ export default {
   },
   components: {
     ReadingView,
-    EditingView,
-    LoginView
+    EditingView
   },
   methods: {
+    async fetchData() {
+        const a = await supabase
+            .from('users')
+            .select()
+            .order('order', { ascending: true })
+        const { data } = a;
+        this.users = data;
+    },
+    addNewUser() {
+      const user = this.session.user;
+      const o = {
+        id: user.id, 
+      };
+      this.insertData(o);
+    },
+    async insertData(oHere) { 
+      try {
+            const { error } = await supabase
+                .from('users')
+                .insert(oHere)
+            if (error) {
+                throw error;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    async loginGoogle() {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google'
+      })
+      
+      console.log("data = ", data);
+      console.log("error = ", error);
+    },
+    async signout() {
+      const { error } = await supabase.auth.signOut()
+      console.log("error = ", error);
+      if(error == null) {
+        alert('로그아웃 되었습니다.')
+        window.location.reload(true); // 리로드 방식 expense를 []로
+      } else {
+        alert('checking..')
+      }
+    },
     sessionListener() {
       supabase.auth.onAuthStateChange((event, session) => {
         console.log("sessionListener = ", event, session)
+
         this.session = session;
+
+        if(session !== null) {
+
+          const userIdNow = this.session.user.id;
+          const usersArray = this.users;
+          
+          const isNew = usersArray.find(eachUser => eachUser.id == userIdNow);
+
+          console.log("isNew =", isNew);
+
+          // this.addNewUser();
+
+          // this.isNewMember();
+
+          // const user = session.user;
+          // console.log("user = ", user);
+
+          // console.log("created_at =", user.created_at);
+          // console.log("updated_at =", user.updated_at);
+
+          // if (user.created_at === user.updated_at) {
+          //   console.log('신규 사용자입니다.');
+          // } else {
+          //   console.log('기존 사용자입니다.');
+          // }
+
+          this.loginMode = false;
+          
+        } else {
+          this.loginMode = true;
+        }
+
       })
+
+    },
+    async isNewMember() {
+      const user = this.session.user;
+      console.log("user @isNewMember =", user);
+      
+      const { data, error } = await supabase
+          .from('users')
+          .select('id')
+          .eq('email', user.email)
+          .single();
+
+        if (error) {
+          console.error('사용자 검색 오류:', error);
+        } else {
+          if (data) {
+            console.log('기존 사용자입니다.');
+          } else {
+            console.log('신규 사용자입니다.');
+          }
+        }
     },
     handlingMode() {
       this.mode = !this.mode
