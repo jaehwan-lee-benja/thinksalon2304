68079543dea3226e1513f5e0794715c65b1f7c5a
jaehwan-lee-benja <designerbenja@gmail.com>
2023-07-20 14:03:25 +0900

    작업 전 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
-  <div :class="appBodyGrid">
+  <div :class="mainBodyDiv">
     <div :class="menuDiv">
       <div :class="titleDiv">
         <h1>현금흐름 관리하기</h1>
       </div>
-      <div :class="pageHanderDiv">
-        <button @click="handlingMode"> 
-          <span v-if="mode">편집하기</span>
-          <span v-else>뒤로가기</span> 
-        </button>
-        <div>
-          <p v-if="mode">'편집하기' 버튼을 누르면, 내용을 편집할 수 있습니다.</p>
-          <p v-else>'뒤로가기' 버튼을 누르면, 편집모드가 종료됩니다.(저장되지 않음)</p>
-        </div>
-        
-      </div>
-      <div :class="loginMenuDiv">
-          <div v-if="loginMode" >
-            <button @click="loginGoogle">구글 로그인</button>
-            <p> *버튼을 눌러 로그인할 수 있습니다.</p>
+      <div :class="subMenuGrid">
+        <div :class="pageHanderDiv">
+          <div v-if="mode">
+            <button @click="handlingMode">편집모드로 전환</button>
+            <span v-if="mode"> 버튼을 누르면, 내용을 편집할 수 있습니다.</span>
           </div>
           <div v-else>
-            <p>로그인 계정: {{ email }}</p>
-            <button @click="signout">로그아웃</button>
+            <button @click="handlingMode">읽기모드로 전환</button>
+            <span> 버튼을 누르면, 편집모드가 종료됩니다.</span>
           </div>
         </div>
+        <div :class="loginMenuDiv">
+            <div v-if="loginMode" >
+              <button @click="loginGoogle">구글 로그인</button>
+              <span> *버튼을 눌러 로그인할 수 있습니다.</span>
+            </div>
+            <div v-else>
+              <span> 로그인 계정: {{ email }} | </span>
+              <button @click="signout">로그아웃</button>
+
+            </div>
+        </div>
+      </div>
+
       
     </div>
-    <div :class="mainViewGrid">
+    <div :class="mainViewDiv">
       <ReadingView v-if="mode" />
       <EditingView v-else />
     </div>
@@ -43,12 +46,13 @@ export default {
   data() {
     return {
       mode: true,
-      appBodyGrid: 'appBodyGrid',
       menuDiv: 'menuDiv',
       pageHanderDiv: 'pageHanderDiv',
       loginMenuDiv: 'loginMenuDiv',
       titleDiv: 'titleDiv',
-      mainViewGrid: 'mainViewGrid'
+      mainViewDiv: 'mainViewDiv',
+      subMenuGrid: 'subMenuGrid',
+      mainBodyDiv: 'mainBodyDiv',
     }
   },
   mixins: [ReadingView],
@@ -58,7 +62,14 @@ export default {
   },
   methods: {
     handlingMode() {
+      console.log("this.mode1 = ", this.mode);
+      if(!this.mode) {
+        confirm("편집모드를 종료하시겠습니까? 수정 후 저장하지 않은 내용은 저장되지 않습니다.")
+      }
+
       this.mode = !this.mode
+      
+      console.log("this.mode2 = ", this.mode);
     },
   }
 }
--- src/views/ReadingView.vue
+++ src/views/ReadingView.vue
@@ -1,114 +1,136 @@
 <template>
   <div>
+    <div :class="unitDiv">
+      <h2>돈이 나가는 영역</h2>
+      <ul>
+        <li v-for="expense in sortTotalExpenses" :key="expense.id">
+          <span>결정값 : </span>
+          <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
+        </li>
+        <li>계산값 : <input :class="amountStyle" v-model="sumTotalExpenses" readonly></li>
+      </ul>
+      <Bar :data="data" :options="options" />
+    </div>
+    <div :class="subGrid">
       <div :class="unitDiv">
-        <h2>돈이 나가는 영역</h2>
-        <ul>
-            <li v-for="expense in sortTotalExpenses" :key="expense.id">
-                <span>결정값 : </span>
-                <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
+        <div>
+          <h3>과거형 지출</h3>
+          <ul>
+            <li v-for="expense in sortPastExpenses" :key="expense.id">
+              <span>결정값 : </span>
+              <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
             </li>
-            <li>계산값 : <input :class="amountStyle" v-model="sumTotalExpenses" readonly></li>
-        </ul>
-      </div>
-      <div :class="subGrid">
-        <div :class="unitDiv">
-            <div>
-            <h3>과거형 지출</h3>
-            <ul>
-                <li v-for="expense in sortPastExpenses" :key="expense.id">
-                    <span>결정값 : </span>
-                    <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
-                </li>
-                <li>계산값 : <input :class="amountStyle" v-model="sumPastExpenses" readonly></li>
-            </ul>
-            </div>
-            <div>
-            <ol>
-                <li v-for="expense in sortParentsPastExpenses" :key="expense.id">
-                    <input :class="categoryStyle" v-model="expense.category" readonly>
-                    <span> : </span>
-                    <input :class="amountStyle" v-model="expense.amount" readonly>
-                </li>
-            </ol>
-            </div>
+            <li>계산값 : <input :class="amountStyle" v-model="sumPastExpenses" readonly></li>
+          </ul>
         </div>
-        <div :class="unitDiv">
-            <div>
-            <h3>현재형 지출</h3>
-            <ul>
-                <li v-for="expense in sortPresentExpenses" :key="expense.id">
-                    <span>결정값 : </span>
-                    <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
-                </li>
-                <li>계산값 : <input :class="amountStyle" v-model="sumPresentExpenses" readonly></li>
-            </ul>
-            </div>
-            <div>
-            <ol>
-                <li v-for="expense in sortParentsPresentExpenses" :key="expense.id">
-                    <input :class="categoryStyle" v-model="expense.category" readonly>
-                    <span> : </span>
-                    <input :class="amountStyle" v-model="expense.amount" readonly>
-                </li>
-            </ol>
-            </div>
-        </div>
-        <div :class="unitDiv">
-            <div>
-            <h3>미래형 지출</h3>
-            <ul>
-                <li v-for="expense in sortFutureExpenses" :key="expense.id">
-                    <span>결정값 : </span>
-                    <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
-                </li>
-                <li>계산값 : <input :class="amountStyle" v-model="sumFutureExpenses" readonly></li>
-            </ul>
-            </div>
-            <div>
-            <ol>
-                <li v-for="expense in sortParentsFutureExpenses" :key="expense.id">
-                    <input :class="categoryStyle" v-model="expense.category" readonly>
-                    <span> : </span>
-                    <input :class="amountStyle" v-model="expense.amount" readonly>
-                </li>
-            </ol>
-            </div>
+        <div>
+          <ol>
+            <li v-for="expense in sortParentsPastExpenses" :key="expense.id">
+              <input :class="categoryStyle" v-model="expense.category" readonly>
+              <span> : </span>
+              <input :class="amountStyle" v-model="expense.amount" readonly>
+            </li>
+          </ol>
         </div>
       </div>
+      <div :class="unitDiv">
+        <div>
+          <h3>현재형 지출</h3>
+          <ul>
+            <li v-for="expense in sortPresentExpenses" :key="expense.id">
+              <span>결정값 : </span>
+              <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
+            </li>
+            <li>계산값 : <input :class="amountStyle" v-model="sumPresentExpenses" readonly></li>
+          </ul>
+        </div>
+        <div>
+          <ol>
+            <li v-for="expense in sortParentsPresentExpenses" :key="expense.id">
+              <input :class="categoryStyle" v-model="expense.category" readonly>
+              <span> : </span>
+              <input :class="amountStyle" v-model="expense.amount" readonly>
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div :class="unitDiv">
+        <div>
+          <h3>미래형 지출</h3>
+          <ul>
+            <li v-for="expense in sortFutureExpenses" :key="expense.id">
+              <span>결정값 : </span>
+              <input :class="amountStyle" v-model="expense.amount" placeholder="0" readonly>
+            </li>
+            <li>계산값 : <input :class="amountStyle" v-model="sumFutureExpenses" readonly></li>
+          </ul>
+        </div>
+        <div>
+          <ol>
+            <li v-for="expense in sortParentsFutureExpenses" :key="expense.id">
+              <input :class="categoryStyle" v-model="expense.category" readonly>
+              <span> : </span>
+              <input :class="amountStyle" v-model="expense.amount" readonly>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-  
+
 import { supabase } from '../lib/supabaseClient.js'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 export default {
 
   data() {
-      return {
+    return {
 
-        expenses: [],
+      data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{ data: [40, 20, 12] }]
+      },
+      options: {
+        responsive: true
+      },
 
-        bodyDiv: 'bodyDiv',
-        unitDiv: 'unitDiv',
-        subGrid: 'subGrid',
+      expenses: [],
 
-        categoryStyle: 'categoryStyle_readingMode',
-        amountStyle: 'amountStyle_readingMode',
-        newUlStyle: 'newUlStyle',
-        saveEditedStyle: 'saveEditedStyle',
+      bodyDiv: 'bodyDiv',
+      unitDiv: 'unitDiv',
+      subGrid: 'subGrid',
 
-        isNewUser: '',
-        session: '',
-        loginMode: true,
-        email: ''
+      categoryStyle: 'categoryStyle_readingMode',
+      amountStyle: 'amountStyle_readingMode',
+      newUlStyle: 'newUlStyle',
+      saveEditedStyle: 'saveEditedStyle',
 
-      }
+      isNewUser: '',
+      session: '',
+      loginMode: true,
+      email: ''
+
+    }
   },
   mounted() {
     this.fetchData(),
-    this.sessionListener()
+      this.sessionListener()
   },
   computed: {
 
@@ -137,23 +159,23 @@ export default {
 
     sumTotalExpenses() {
       return this.expenses.filter(e => e.parentsCategory === "total")
-      .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
-      .toLocaleString();
+        .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
+        .toLocaleString();
     },
     sumPastExpenses() {
       return this.expenses.filter(e => e.parentsCategory === "past")
-      .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
-      .toLocaleString();
+        .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
+        .toLocaleString();
     },
     sumPresentExpenses() {
       return this.expenses.filter(e => e.parentsCategory === "present")
-      .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
-      .toLocaleString();
+        .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
+        .toLocaleString();
     },
     sumFutureExpenses() {
       return this.expenses.filter(e => e.parentsCategory === "future")
-      .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
-      .toLocaleString();
+        .reduce((acc, item) => acc + Number(item.amount.replace(/,/g, "")), 0)
+        .toLocaleString();
     }
   },
   methods: {
@@ -167,7 +189,7 @@ export default {
     async signout() {
       const { error } = await supabase.auth.signOut()
       console.log("error = ", error);
-      if(error == null) {
+      if (error == null) {
         alert('로그아웃 되었습니다.')
         window.location.reload(true); // 리로드 방식 expense를 []로
       } else {
@@ -176,46 +198,46 @@ export default {
     },
     async fetchData() {
 
-        const fetchedData = await supabase
-            .from('expense')
-            .select()
-            .order('order', { ascending: true })
-        const { data } = fetchedData;
-        
-        const session = this.session;
+      const fetchedData = await supabase
+        .from('expense')
+        .select()
+        .order('order', { ascending: true })
+      const { data } = fetchedData;
 
-        let userId = "";
+      const session = this.session;
 
-        if(session !== null) {
-          userId = session.user.id;
-        } else {
-          userId = null;
+      let userId = "";
+
+      if (session !== null) {
+        userId = session.user.id;
+      } else {
+        userId = null;
+      }
+
+      function whatId(el) {
+        if (el.user_id === userId) {
+          return true
         }
+      }
 
-        function whatId(el) {
-          if(el.user_id === userId) {
-            return true
-          }
-        }
+      const dataById = fetchedData.data.filter(whatId);
+      const dataLength = dataById.length;
 
-        const dataById = fetchedData.data.filter(whatId);
-        const dataLength = dataById.length;
+      if (dataLength > 0) {
+        this.isNewUser = false;
+      } else {
+        this.isNewUser = true;
+      }
 
-        if(dataLength > 0) {
-          this.isNewUser = false;
-        } else {
-          this.isNewUser = true;
-        }
+      if (this.isNewUser && session !== null) {
+        this.insertInitailData();
+      }
 
-        if(this.isNewUser && session !== null) {
-          this.insertInitailData();
-        }
-
-        data.forEach(e => e.amount = e.amount.toLocaleString());
-        this.expenses = data;
+      data.forEach(e => e.amount = e.amount.toLocaleString());
+      this.expenses = data;
 
     },
-    insertInitailData(){
+    insertInitailData() {
       const initialDataArray = [
         {
           id: this.getUuidv4(),
@@ -252,17 +274,17 @@ export default {
       ]
       initialDataArray.forEach(eachData => this.insertData(eachData));
     },
-    async insertData(dataHere) { 
+    async insertData(dataHere) {
       try {
-            const { error } = await supabase
-                .from('expense')
-                .insert(dataHere)
-            if (error) {
-                throw error;
-            }
-        } catch (error) {
-            console.error(error);
+        const { error } = await supabase
+          .from('expense')
+          .insert(dataHere)
+        if (error) {
+          throw error;
         }
+      } catch (error) {
+        console.error(error);
+      }
     },
     async getSession() {
       const { data, error } = await supabase.auth.getSession()
@@ -280,21 +302,24 @@ export default {
     },
     loginBtnHandler() {
       const session = this.session;
-      if(session !== null) {
+      if (session !== null) {
         this.loginMode = false;
       } else {
         this.loginMode = true;
       }
     },
     getUuidv4() {
-        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        );
+      );
     },
+  },
+  components: {
+    Bar
   }
 }
 </script>
 
 <style scoped>
-  @import '../style.css';
+@import '../style.css';
 </style>
