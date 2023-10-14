33eb2f9a7aba7bcb5fe1a23d4795027868174c8c
jaehwan <designerbenja@gmail.com>
2023-10-15 07:45:40 +0900

    version CRUD basic done

--- src/views/VersionList.vue
+++ src/views/VersionList.vue
@@ -1,12 +1,24 @@
 <template>
-    <input v-model="this.newVersionTitle" placeholder="새 타이틀 적기">
-    <button @click="upsertVersion">만들기</button>
-    <ol>
-        <li v-for="version in this.expenseVersions" :key="version.index">
-            <input v-model="version.version_title">
-            <input type="checkbox" v-model="checkedValues" @change="updateCheckedVersion" :id="version.id" :true-value="yes" :false-value="no">
-        </li>
-    </ol>
+    <h2>파일 편집하기</h2>
+
+    <div>
+        <h4>새 파일 만들기</h4>
+        <span>새 파일명: </span>
+        <input v-model="this.newVersionTitle" placeholder="새 타이틀 적기">
+        <button @click="createNewVersion">만들기</button>
+    </div>
+    
+    <div>
+        <h4>기존 파일 수정하기</h4>
+        <ol>
+            <li v-for="version in this.expenseVersions" :key="version.index">
+                <input type="checkbox" v-model="checkedValues" @change="updateCheckedVersion" :id="version.id" :true-value="yes" :false-value="no">
+                <input v-model="version.version_title">
+                <button @click="upsertVersion(version)">수정하기</button>
+                <button @click="deleteVersion(version.id)">삭제하기</button>
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script>
@@ -39,7 +51,6 @@ export default {
                 .select()
             const { data } = a;
             this.expenseVersions = data;
-            console.log("this.expenseVersions = ", this.expenseVersions);
         },
         updateCheckedVersion(event) {
             console.log("event.target =", event.target)
@@ -52,17 +63,32 @@ export default {
             }
           }
         },
-        async upsertVersion() {
-
+        createNewVersion() {
             const o = { 
                 id: this.getUuidv4(),
                 version_title: this.newVersionTitle,
             }
+            this.upsertVersion(o);
+        },
+        async upsertVersion(oHere) {
 
             try {
                 const { error } = await supabase
                     .from('expense_version')
-                    .upsert(o)
+                    .upsert(oHere)
+                if (error) {
+                    throw error;
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async deleteVersion(versionIdHere) {
+            try {
+                const { error } = await supabase
+                    .from('expense_version')
+                    .delete()
+                    .eq('id', versionIdHere)
                 if (error) {
                     throw error;
                 }
