d79745692e3970a90557cef9a130ac097c88ee23
jaehwan-lee-benja <designerbenja@gmail.com>
2023-11-04 16:03:19 +0900

    css refactorying..

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div :class="mainBodyDiv">
-    <div :class="titleDiv">
-      <h1>현금흐름 관리하기</h1>
-    </div>
-    <div :class="subMenuGrid">
-      <div :class="loginMenuDiv">
-          <div v-if="loginMode" >
-            <button @click="loginGoogle">구글 로그인</button>
-            <span> *버튼을 눌러 로그인할 수 있습니다.</span>
-          </div>
-          <div v-else>
-            <span> 로그인 계정: {{ email }} | </span>
-            <button @click="signout">로그아웃</button>
-
-          </div>
+  <div :class="bodyDiv">
+    <div :class="menuMainGrid">
+      <div :class="menuDiv">
       </div>
-    </div>
-    <div>
-      <ListEditingView />
+      <div :class="mainDiv">
+        <div :class="loginMenuDiv">
+            <div v-if="loginMode" >
+              <button @click="loginGoogle">구글 로그인</button>
+              <span> *버튼을 눌러 로그인할 수 있습니다.</span>
+            </div>
+            <div v-else>
+              <span> 로그인 계정: {{ email }} | </span>
+              <button @click="signout">로그아웃</button>
+
+            </div>
+        </div>
+        <div>
+          <ListEditingView />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,9 +31,9 @@ export default {
   data() {
     return {
       loginMenuDiv: 'loginMenuDiv',
-      titleDiv: 'titleDiv',
-      subMenuGrid: 'subMenuGrid',
-      mainBodyDiv: 'mainBodyDiv',
+      menuMainGrid: 'menuMainGrid',
+      bodyDiv: 'bodyDiv',
+      menuDiv: 'menuDiv'
     }
   },
   mixins: [LogIn],
